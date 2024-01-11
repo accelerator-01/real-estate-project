5f88e95110cbb3e8cb3de6d68a8c4a839a01a1db
accelerator-01 <acceleratorgame01@gmail.com>
2024-01-11 21:21:46 +0700

    testing

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,6 @@ import {
   CardBody,
   CardFooter,
   Typography,
-  Tooltip,
 } from "@material-tailwind/react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -13,108 +12,53 @@ function Contact() {
   return (
     <div className="container mx-auto">
       <Header />
-      <div className="flex flex-wrap justify-center gap-5">
-        <Card className="w-96">
-          <CardHeader floated={false} className="h-80">
+      <div className="flex flex-wrap justify-center gap-5 py-10">
+        <Card className=" w-96">
+          <CardHeader className="relative h-56">
+            <h1 className="text-center font-bold mb-5">AHMAD FIRDAUS</h1>
             <img
-              src="https://docs.material-tailwind.com/img/team-3.jpg"
-              alt="profile-picture"
+              src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+              alt="card-image"
             />
           </CardHeader>
-          <CardBody className="text-center">
-            <Typography variant="h4" color="blue-gray" className="mb-2">
-              Natalie Paisley
+          <CardBody>
+            <Typography className="mb-2 text-center font-bold">
+              Web Developer
             </Typography>
-            <Typography color="blue-gray" className="font-medium" textGradient>
-              CEO / Co-Founder
+            <Typography className="text-justify">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
+              earum quaerat, nihil totam deleniti maxime ad exercitationem enim
+              asperiores ab corrupti dicta atque?
             </Typography>
           </CardBody>
-          <CardFooter className="flex justify-center gap-7 pt-2">
-            <Tooltip content="Like">
-              <Typography
-                as="a"
-                href="#facebook"
-                variant="lead"
-                color="blue"
-                textGradient
-              >
-                <i className="fab fa-facebook" />
-              </Typography>
-            </Tooltip>
-            <Tooltip content="Follow">
-              <Typography
-                as="a"
-                href="#twitter"
-                variant="lead"
-                color="light-blue"
-                textGradient
-              >
-                <i className="fab fa-twitter" />
-              </Typography>
-            </Tooltip>
-            <Tooltip content="Follow">
-              <Typography
-                as="a"
-                href="#instagram"
-                variant="lead"
-                color="purple"
-                textGradient
-              >
-                <i className="fab fa-instagram" />
-              </Typography>
-            </Tooltip>
+          <CardFooter className="pt-0 justify-between flex">
+            <span>Wa</span>
+            <span>IG</span>
           </CardFooter>
         </Card>
-
-        <Card className="w-96">
-          <CardHeader floated={false} className="h-80">
+        <Card className=" w-96">
+          <CardHeader className="relative h-56">
+            <h1 className="text-center font-bold mb-5">
+              ALFI KUKUH ADHAR WICAKSANA
+            </h1>
             <img
-              src="https://docs.material-tailwind.com/img/team-3.jpg"
-              alt="profile-picture"
+              src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+              alt="card-image"
             />
           </CardHeader>
-          <CardBody className="text-center">
-            <Typography variant="h4" color="blue-gray" className="mb-2">
-              Natalie Paisley
+          <CardBody>
+            <Typography className="mb-2 text-center font-bold">
+              Digital Marketing
             </Typography>
-            <Typography color="blue-gray" className="font-medium" textGradient>
-              CEO / Co-Founder
+            <Typography className="text-justify">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
+              earum quaerat, nihil totam deleniti maxime ad exercitationem enim
+              asperiores ab corrupti dicta atque?
             </Typography>
           </CardBody>
-          <CardFooter className="flex justify-center gap-7 pt-2">
-            <Tooltip content="Like">
-              <Typography
-                as="a"
-                href="#facebook"
-                variant="lead"
-                color="blue"
-                textGradient
-              >
-                <i className="fab fa-facebook" />
-              </Typography>
-            </Tooltip>
-            <Tooltip content="Follow">
-              <Typography
-                as="a"
-                href="#twitter"
-                variant="lead"
-                color="light-blue"
-                textGradient
-              >
-                <i className="fab fa-twitter" />
-              </Typography>
-            </Tooltip>
-            <Tooltip content="Follow">
-              <Typography
-                as="a"
-                href="#instagram"
-                variant="lead"
-                color="purple"
-                textGradient
-              >
-                <i className="fab fa-instagram" />
-              </Typography>
-            </Tooltip>
+          <CardFooter className="pt-0 flex justify-between">
+            <span>Wa</span>
+            <span>IG</span>
           </CardFooter>
         </Card>
       </div>
